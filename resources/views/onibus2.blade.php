a7964c44fcf32455fc15abb00fca47835007e286
Tiago Rios da Rocha <tiagoriosrocha@gmail.com>
2023-08-24 18:31:14 -0300

    coloquei https para css

--- resources/views/onibus2.blade.php
+++ resources/views/onibus2.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="css/index.css">
     <!--Bootstrap usado-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!--Script usado-->
     <script src="js/index.js"></script>
